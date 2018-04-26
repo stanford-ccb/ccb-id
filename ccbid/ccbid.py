96b50e6c980ec3a1936cddf60e64fc402bfdf344
christobal54 <cbanderson08@gmail.com>
2018-04-25 18:35:45 -0700

    added first commits with stupid recursive importing that ill fix later

---
+++ ccbid/ccbid.py
@@ -0,0 +1,15 @@
+import pickle as _pickle
+import numpy as _np
+import pandas as _pd
+from sklearn import svm as _svm
+from sklearn import metrics as _metrics
+from sklearn import ensemble as _ensemble
+from sklearn import multiclass as _multiclass
+from sklearn import calibration as _calibration
+from sklearn import preprocessing as _preprocessing
+from sklearn import model_selection as _model_selection
+from sklearn.decomposition import PCA as _PCA
+from matplotlib import pyplot as _plt
+
+def dummy():
+    pass
