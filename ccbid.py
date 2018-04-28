f7abd9218682b2986b51b1239e65593c76adebba
christobal54 <cbanderson08@gmail.com>
2018-04-27 22:13:07 -0700

    added sample weights function, cleaned up modeling functionality

--- ccbid.py
+++ ccbid.py
@@ -3,7 +3,7 @@ import pandas as _pd
 import gdal as _gdal
 import copy as _copy
 import pickle as _pickle
-from sklearn import svm as _svm
+from sklearn import utils as _utils
 from sklearn import metrics as _metrics
 from sklearn import ensemble as _ensemble
 from sklearn import multiclass as _multiclass
@@ -30,12 +30,12 @@ class read:
         (based on ccb-id/suport_files/neon-bands.csv')
         
         Args:
-            path - the path to the wavelength file
+            path        - the path to the wavelength file
             
         Returns:
             list of [wavelengths, good_bands]
             wavelengths - an array with the wavelengths associated with each band
-            good_bands - a boolean array for bands to include in analysis (True = good)
+            good_bands  - a boolean array for bands to include in analysis (True = good)
         """
         df = _pd.read_csv(path)
         wavelengths = _np.array(df['Wavelength'])
@@ -50,12 +50,12 @@ class read:
         (based on ccb-id/suport_files/species_id.csv)
         
         Args:
-            path - the path to the species id file
+            path         - the path to the species id file
             
         Returns:
             list of [crown_id, species_id, species_name]
-            crown_id - an array with the unique crown identities
-            species_id - an array with the species ID codes per crown
+            crown_id     - an array with the unique crown identities
+            species_id   - an array with the species ID codes per crown
             species_name - an array with the species names per crown
         """
         df = _pd.read_csv(path)
@@ -71,12 +71,12 @@ class read:
         (based on ccb-id/suport_files/species_id.csv)
         
         Args:
-            path - the path to the genus id file
+            path       - the path to the genus id file
             
         Returns:
             list of [crown_id, genus_id, genus_name]
-            crown_id - an array with the unique crown identities
-            genus_id - an array with the genus ID codes per crown
+            crown_id   - an array with the unique crown identities
+            genus_id   - an array with the genus ID codes per crown
             genus_name - an array with the genus names per crown
         """
         df = _pd.read_csv(path)
@@ -92,7 +92,7 @@ class read:
         (based on ccb-id/support_files/training.csv)
         
         Args:
-            path - the path to the training data csv file
+            path     - the path to the training data csv file
             
         Returns:
             list of [crown_id, features]
@@ -124,7 +124,7 @@ class read:
             the object stored in the pickle file
         """
         with open(path, 'r') as f:
-            return pickle.load(f)
+            return _pickle.load(f)
     
     @staticmethod
     def neon_bands():
@@ -137,14 +137,14 @@ def match_species_ids(crown_id, label_id, labels):
     Args:
         crown_id - an array of crown IDs from the training data
         label_id - an array of crown IDs associated with the labeled data
-        labels - an array of labels (e.g., species names, species codes)
-                 label_id and labels should be of the same size
+        labels   - an array of labels (e.g., species names, species codes)
+                   label_id and labels should be of the same size
                  
     Returns:
         [unique_labels, unique_crowns, crown_labels]
         unique_labels - a list of the unique entities from the input labels variable
         unique_crowns - a list of the unique crown entities from the crown_id variable
-        crown_labels - an array with the labels aligned with the original shape of crown_id
+        crown_labels  - an array with the labels aligned with the original shape of crown_id
     """
     # get the unique labels and crown id's
     unique_labels = _np.unique(labels)
@@ -184,7 +184,7 @@ def write_pck(path, variable):
     """Writes a python/pickle format data file
     
     Args:
-       path - the path to the output pickle file
+       path     - the path to the output pickle file
        variable - the python variable to write
            
     Returns:
@@ -206,13 +206,13 @@ class identify_outliers:
         
         Args:
             features - the input feature data for finding outliers
-            n_pcs - the number of principal components to look for outliers in
-            thresh - the standard-deviation multiplier for outlier id 
-                     (e.g. thresh = 3 means values > 3 stdv from the mean will 
-                     be flagged as outliers)
+            n_pcs    - the number of principal components to look for outliers in
+            thresh   - the standard-deviation multiplier for outlier id 
+                       (e.g. thresh = 3 means values > 3 stdv from the mean will 
+                       be flagged as outliers)
         
         Returns:
-            mask - a boolean array with True for good values, False for outliers
+            mask     - a boolean array with True for good values, False for outliers
         """
         # create the bool mask where outlier samples will be flagged as False
         mask = _np.repeat(True, features.shape[0])
@@ -229,24 +229,55 @@ class identify_outliers:
         return mask
 
 
+class transform:
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def pca(features, n_pcs=100):
+        """PCA transformation function
+        
+        Args:
+            features - the input feature data to transform
+            n_pcs    - the number of components to keep after transformation
+            
+        Returns:
+            an array of PCA-transformed features
+        """
+        reducer = _PCA(n_components=n_pcs, whiten=True)
+        return reducer.fit_transform(features)
+        
+    @staticmethod
+    def from_path(path, features, n_features=None):
+        """Transformation using a saved decomposition object
+        
+        Args:
+            path       - the path to the saved decomposition object
+            features   - the input feature data to transform
+            n_features - the number of features to keep after transformation
+        """
+        reducer = read.pck(path)
+        transformed = reducer.fit_transform(features)
+        return transformed[:, 0:n_features]
+
 class resample:
     def __init__(self):
         pass
     
     @staticmethod
-    def uniform(features, crown_labels, n_per_class=400):
+    def uniform(features, crown_labels, n_per_class=400, other_array=None):
         """Performs a random uniform resampling of each class to a fixed number of samples
         
         Args:
-            features - the feature data to evenly resample
+            features     - the feature data to evenly resample
             crown_labels - labels that correspond to each sample in the feature
                            data and define the unique IDs to resample from
-            n_per_class - the number of samples to select per class
+            n_per_class  - the number of samples to select per class
             
         Returns:
             list of [resample_x, resample_y]
-            resample_x - the feature data resampled with shape (n_lables * n_per_class, n_features)
-            resample_y - the class labels from 0 to n_unique_labels with shape (n_lables * n_per_class)
+            resample_x   - the feature data resampled with shape (n_lables * n_per_class, n_features)
+            resample_y   - the class labels from 0 to n_unique_labels with shape (n_lables * n_per_class)
         """
         # get the unique species labels for balanced-class resampling
         unique_labels = _np.unique(crown_labels)
@@ -255,6 +286,12 @@ class resample:
         # set up the x and y variables for storing outputs
         resample_x = _np.zeros((n_labels*n_per_class, features.shape[1]))
         resample_y = _np.zeros(n_labels*n_per_class, dtype=_np.uint8)
+        
+        if other_array is not None:
+            if other_array.ndim == 1:
+                resample_o = _np.zeros(n_labels*n_per_class)
+            else:
+                resample_o = _np.zeros((n_labels*n_per_class, other_array.shape[1]))
         
         # loop through and randomly sample each species
         for i in range(n_labels):
@@ -265,7 +302,13 @@ class resample:
             resample_x[i*n_per_class:(i+1)*n_per_class] = features[ind_class[0][ind_randm]]
             resample_y[i*n_per_class:(i+1)*n_per_class] = i
             
-        return [resample_x, resample_y]
+            if other_array is not None:
+                resample_o[i*n_per_class:(i+1)*n_per_class] = other_array[ind_class[0][ind_randm]]
+            
+        if other_array is None:
+            return [resample_x, resample_y]
+        else:
+            return [resample_x, resample_y, resample_o]
         
     
 class crown_ensemble:
@@ -277,6 +320,31 @@ class crown_ensemble:
         pass
     
 
+def get_sample_weights(y):
+    """Calculates the balanced sample weights for a set of unique classes
+    
+    Args:
+        y - the input class labels
+        
+    Returns:
+        weights_sample - an array of length (y) with the per-class weights per sample
+    """
+    # get the unique classes in the array
+    classes = _np.unique(y)
+    n_classes = len(classes)
+    
+    # calculate the per-class weights
+    weights_class = _utils.class_weight.compute_class_weight('balanced', classes, y)
+    
+    # create and return an array the same dimensions as the input y vector
+    weights_sample = _np.zeros(len(y))
+    for i in range(n_classes):
+        ind_y = y == classes[i]
+        weights_sample[ind_y] = weights_class[i]
+        
+    return weights_sample
+
+    
 #-----
 # functions to handle the CCB-ID classification models
 #-----
@@ -407,7 +475,7 @@ class model:
             average_proba  - flag to report the output probabilities as the average across models
         """
         if average_proba:
-            self.average_proba = True
+            self.average_proba_ = True
             
         for i in range(self.n_models_):
             if use_calibrated:
