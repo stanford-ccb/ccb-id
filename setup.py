4d79b975dda52c3b12a1f3d7b7706a62f1a0c194
christobal54 <cbanderson08@gmail.com>
2018-04-30 11:24:27 -0700

    updated to less of a cargo-culted setup.py file

--- setup.py
+++ setup.py
@@ -1,55 +1,41 @@
-# this script was pulled from the following link
-#  https://jeffknupp.com/blog/2013/08/16/open-sourcing-a-python-project-the-right-way/
-# and I'm not totally sure it is set up to work that well with the package 
-# in its current form. We'll see!
-
-from __future__ import print_function
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
-import io
-import codecs
-import os
-import sys
-
 import ccbid
 
-here = os.path.abspath(os.path.dirname(__file__))
+setup_args = {
+    'name': 'ccb-id',
+    'version': ccbid.__version__,
+    'url': 'https://github.com/stanford-ccb/ccb-id',
+    'license': 'MIT',
+    'author': 'Christopher Anderson',
+    'author_email': 'cbanders@stanford.edu',
+    'description': 'Species classification approach using imaging spectroscopy',
+    'keywords': [
+        'neon',
+        'biogeography',
+        'classification',
+        'machine learning',
+        'imaging spectroscopy',
+        'remote sensing'
+    ],
+    'packages': ['ccbid'],
+    'include_package_data': True,
+    'platforms': 'any',
+    'scripts': [
+        'bin/train.py'
+    ],
+    'data_files': [
+        ('support_files', [
+            'support_files/gbc.pck',
+            'support_files/rfc.pck',
+            'support_files/params-gbc.pck',
+            'support_files/params-rfc.pck',
+            'support_files/reducer.pck',
+            'support_files/training.csv',
+            'support_files/testing.csv',
+            'support_files/neon-bands.csv',
+            'support_files/species_id.csv'
+        ])
+    ]
+}
 
-def read(*filenames, **kwargs):
-    encoding = kwargs.get('encoding', 'utf-8')
-    sep = kwargs.get('sep', '\n')
-    buf = []
-    for filename in filenames:
-        with io.open(filename, encoding=encoding) as f:
-            buf.append(f.read())
-    return sep.join(buf)
-
-#long_description = read('README.txt')
-
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        errcode = pytest.main(self.test_args)
-        sys.exit(errcode)
-
-setup(
-    name='ccb-id',
-    version=ccbid.__version__,
-    url='https://github.com/stanford-ccb/ccb-id/',
-    license='MIT',
-    author='Christopher Anderson',
-    tests_require=['pytest'],
-    cmdclass={'test': PyTest},
-    author_email='cbanders@stanford.edu',
-    description='Species classification approach using imaging spectroscopy',
-    long_description=long_description,
-    packages=['ccbid'],
-    include_package_data=True,
-    platforms='any',
-    scripts=['bin/train.py']
-)
+setup(**setup_args)
